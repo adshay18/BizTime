60c4aeaaa7d26567418f55542672937e1b3c8641
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-04-12 16:13:41 -0400

    enable industries

--- data.sql
+++ data.sql
@@ -19,12 +19,42 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries (
+    code text PRIMARY KEY,
+    name text NOT NULL UNIQUE
+);
+
+CREATE TABLE company_industries (
+  id serial PRIMARY KEY,
+  ind_code text NOT NULL REFERENCES industries ON DELETE CASCADE,
+  comp_code text NOT NULL REFERENCES companies ON DELETE CASCADE
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
-         ('ibm', 'IBM', 'Big blue.');
+         ('ibm', 'IBM', 'Big blue.'),
+         ('bosch', 'Bosch', 'Manufacturer of car parts');
 
-INSERT INTO invoices (comp_Code, amt, paid, paid_date)
+INSERT INTO invoices (comp_code, amt, paid, paid_date)
   VALUES ('apple', 100, false, null),
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries 
+  VALUES  ('tech', 'Technology'),
+          ('cpu', 'Computers'),
+          ('fin', 'Finance'),
+          ('auto', 'Automotive'),
+          ('manf', 'Manufacturing');
+
+INSERT INTO company_industries (ind_code, comp_code)
+  VALUES  ('tech', 'apple'),
+          ('tech', 'ibm'),
+          ('cpu', 'apple'),
+          ('cpu', 'apple'),
+          ('tech', 'bosch'),
+          ('auto', 'bosch'),
+          ('manf', 'apple'),
+          ('manf', 'ibm'),
+          ('manf', 'bosch');
---
+++ routes/industries.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const ExpressError = require('../expressError');
+const router = express.Router();
+const db = require('../db');
+
+router.get('/', async (req, res, next) => {
+	try {
+		const results = await db.query(`SELECT * FROM industries`);
+		return res.json({ industries: results.rows });
+	} catch (e) {
+		return next(e);
+	}
+});
+
+// router.get('/:id', async (req, res, next) => {
+// 	try {
+// 		const { id } = req.params;
+// 		const results = await db.query('SELECT * FROM invoices WHERE id = $1', [ id ]);
+// 		if (results.rows.length === 0) {
+// 			throw new ExpressError(`Can't find invoice with id of ${id}`, 404);
+// 		}
+// 		return res.send({ invoice: results.rows[0] });
+// 	} catch (e) {
+// 		return next(e);
+// 	}
+// });
+
+// // Add new invoice using JSON request
+// router.post('/', async (req, res, next) => {
+// 	try {
+// 		const { comp_code, amt } = req.body;
+// 		const results = await db.query('INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING *', [
+// 			comp_code,
+// 			amt
+// 		]);
+// 		return res.status(201).json({ invoice: results.rows[0] });
+// 	} catch (e) {
+// 		return next(e);
+// 	}
+// });
+
+// router.put('/:id', async (req, res, next) => {
+// 	try {
+// 		const { id } = req.params;
+// 		const { amt, paid } = req.body;
+// 		if (paid === true) {
+// 			const results = await db.query(
+// 				'UPDATE invoices SET amt=$2, paid=$3, paid_date=CURRENT_DATE WHERE id=$1 RETURNING *',
+// 				[ id, amt, paid ]
+// 			);
+// 			if (results.rows.length === 0) {
+// 				throw new ExpressError(`Can't update invoice with id of ${id}, invoice not found.`, 404);
+// 			}
+// 			return res.send({ invoice: results.rows[0] });
+// 		} else {
+// 			const results = await db.query(
+// 				'UPDATE invoices SET amt=$2, paid=$3, paid_date=null WHERE id=$1 RETURNING *',
+// 				[ id, amt, paid ]
+// 			);
+// 			return res.send({ invoice: results.rows[0] });
+// 		}
+// 	} catch (e) {
+// 		return next(e);
+// 	}
+// });
+
+// router.delete('/:id', async (req, res, next) => {
+// 	try {
+// 		const results = db.query('DELETE FROM invoices WHERE id = $1', [ req.params.id ]);
+// 		return res.send({ msg: 'DELETED!' });
+// 	} catch (e) {
+// 		return next(e);
+// 	}
+// });
+
+module.exports = router;
